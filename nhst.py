672a844a029fbdb3186e8090ca263e67ae68ab28
BrandonHoeft <brandonmhoeft@gmail.com>
2019-11-17 18:24:36 -0600

    added draw_n_bootstrap_replicates()

--- nhst.py
+++ nhst.py
@@ -2,10 +2,23 @@ import numpy as np
 
 def one_bootstrap_replicate(data, func):
     """
-    Generate 1 bootstrap replicate by applying a function of your choice
-    to one bootstrap resampling of a a one-dimensional array. 
+    Generate 1 bootstrap replicate computing your desired statistic
+    by applying a function of your choice to 1 bootstrap resampling 
+    of a 1D array of data. 
     """
     return func(np.random.choice(data, size=len(data)))
 
 
-
+def draw_n_bootstrap_replicates(data, func, n=1):
+    """
+    Draw N bootstrap replicates, given a 1D array of data and
+    a function of interest.
+    """
+    #Initialize array of replicates: bs_replicates
+    boot_replicates = np.empty(n)
+    
+    #Generate replicates
+    for i in range(n):
+        boot_replicates[i] = one_bootstrap_replicate(data=data, func=func)
+        
+    return boot_replicates
